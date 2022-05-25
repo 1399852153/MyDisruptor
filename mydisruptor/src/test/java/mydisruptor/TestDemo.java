242f019a3eb47adf3b6141ab8e3c5d8841c4ddbf
shanreng <1399852153@qq.com>
2022-05-25 10:47:23 +0800

    项目初始化

---
+++ mydisruptor/src/test/java/mydisruptor/TestDemo.java
@@ -0,0 +1,12 @@
+package mydisruptor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDemo {
+
+    @Test
+    public void testDemo(){
+        Assert.assertEquals(1,1);
+    }
+}
